62705ede5a5de256ef2c8153500a30dc2c4f7365
sephanya2 <sebhanya.141743@marwadiuniversity.ac.in>
2026-02-27 00:11:00 +0530

    Time: 14 ms (63.41%) | Memory: 12.4 MB (92.28%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        m, n = len(s), len(p)
+        
+        # dp[i][j] means s[0:i] matches p[0:j]
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        
+        # Empty string matches empty pattern
+        dp[0][0] = True
+        
+        # Handle patterns like a*, a*b*, a*b*c*
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                
+                # Case 1: Characters match OR pattern has '.'
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                
+                # Case 2: Pattern has '*'
+                elif p[j - 1] == '*':
+                    
+                    # Treat '*' as zero occurrence
+                    dp[i][j] = dp[i][j - 2]
+                    
+                    # If preceding char matches
+                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+        
+        return dp[m][n]
