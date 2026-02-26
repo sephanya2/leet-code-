b592d6678690713acfd23184f49afaf35d153e0d
sephanya2 <sebhanya.141743@marwadiuniversity.ac.in>
2026-02-27 00:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (56.45%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        
+        result = []
+        
+        def backtrack(index, path):
+            # If the combination is complete
+            if index == len(digits):
+                result.append(path)
+                return
+            
+            possible_letters = phone[digits[index]]
+            
+            for letter in possible_letters:
+                backtrack(index + 1, path + letter)
+        
+        backtrack(0, "")
+        return result
